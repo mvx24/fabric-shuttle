8192ad43ae7d866906acca2da6031fdac5091732
marc <marc@symbioticsoftware.com>
2016-12-23 00:25:53 -0800

    Added cron jobs for snowplow and downloading the git repo for access to enrichments.

--- shuttle/services/snowplow.py
+++ shuttle/services/snowplow.py
@@ -2,60 +2,65 @@ import copy
 import json
 import os
 import StringIO
-import urllib2
 import yaml
 
 from fabric.api import cd, put, settings, sudo
 from fabric.context_managers import shell_env
 from fabric.contrib.files import exists
 
-from .cron import add_crontab_section
+from .cron import add_crontab_section, CronSchedule, CronJob
 from .service import Service
 from ..hooks import hook
-from ..shared import apt_get_install, pip_install, red
+from ..shared import apt_get_install, pip_install, red, chown
 
 _PACKAGE_URL = 'http://dl.bintray.com/snowplow/snowplow-generic/snowplow_emr_r77_great_auk.zip'
-_CREATE_TABLE_URL = 'https://raw.githubusercontent.com/snowplow/snowplow/master/4-storage/postgres-storage/sql/atomic-def.sql'
+_MASTER_URL = 'https://codeload.github.com/snowplow/snowplow/zip/master'
+_MASTER_FILE = 'snowplow-master.zip'
+
 _INSTALL_DIR = '/opt/snowplow'
+_MASTER_DIR = os.path.join(_INSTALL_DIR, 'snowplow-master')
 _RUNNER_PATH = os.path.join(_INSTALL_DIR, 'snowplow-emr-etl-runner')
 _LOADER_PATH = os.path.join(_INSTALL_DIR, 'snowplow-storage-loader')
 _CONFIG_PATH = os.path.join(_INSTALL_DIR, 'config.yml')
 _RESOLVER_PATH = os.path.join(_INSTALL_DIR, 'iglu_resolver.json')
+_CREATE_TABLE_PATH = os.path.join(_MASTER_DIR, '4-storage/postgres-storage/sql/atomic-def.sql')
+_ENRICHMENTS_PATH = os.path.join(_MASTER_DIR, '3-enrich/config/enrichments')
 _DEFAULT_RESOLVER = {
-	"schema": "iglu:com.snowplowanalytics.iglu/resolver-config/jsonschema/1-0-0",
-	"data": {
-		"cacheSize": 500,
-		"repositories": [
+	'schema': 'iglu:com.snowplowanalytics.iglu/resolver-config/jsonschema/1-0-0',
+	'data': {
+		'cacheSize': 500,
+		'repositories': [
 			{
-				"name": "Iglu Central",
-				"priority": 0,
-				"vendorPrefixes": [ "com.snowplowanalytics" ],
-				"connection": { "http": { "uri": "http://iglucentral.com" } }
+				'name': 'Iglu Central',
+				'priority': 0,
+				'vendorPrefixes': [ 'com.snowplowanalytics' ],
+				'connection': { 'http': { 'uri': 'http://iglucentral.com' } }
 			}
 		]
 	}
 }
 _CRONTAB_USER = 'root'
 _CRONTAB_SECTION = '[snowplow]'
-_RUNNER_COMMAND = ' '.join((_RUNNER_PATH,'--config', _CONFIG_PATH, '--resolver', _RESOLVER_PATH, '--skip shred elasticsearch'))
-_LOADER_COMMAND = ' '.join((_LOADER_PATH,'--config', _CONFIG_PATH, '--resolver', _RESOLVER_PATH, '--skip shred elasticsearch'))
+_RUNNER_COMMAND = ' '.join((_RUNNER_PATH, '--config', _CONFIG_PATH, '--resolver', _RESOLVER_PATH))
+_LOADER_COMMAND = ' '.join((_LOADER_PATH, '--config', _CONFIG_PATH))
 
 _DEFAULT_SETTINGS = {
-	'runner_schedule': '',
-	'loader_schedule': '',
-	'shred': False,
-	'elasticsearch': False
+	'schedule': CronSchedule(45, 2),
+	'chain': None,
+	'enrichments': _ENRICHMENTS_PATH,
+	'runner_skip': (),
+	'loader_skip': ()
 }
 
 def _config_postgres(target):
 	# Assumes that the target database is already installed, running, and setup with the correct credentials but will try to create both the database and table
-	response = urllib2.urlopen(_CREATE_TABLE_URL)
 	sql = []
-	for line in response.readlines():
-		line = line.strip()
-		if line.startswith('--'):
-			continue
-		sql.append(line.replace('\t', '').replace('\n', '').replace("'", "\\'"))
+	with open(_CREATE_TABLE_PATH) as f:
+		for line in f.readlines():
+			line = line.strip()
+			if line.startswith('--'):
+				continue
+			sql.append(line.replace('\t', '').replace('\n', '').replace("'", "\\'"))
 	sql = ' '.join(sql)
 
 	# Be sure postgressql client is installed and available
@@ -90,6 +95,8 @@ class Snowplow(Service):
 				with cd(_INSTALL_DIR):
 					sudo('wget --no-clobber %s' % _PACKAGE_URL)
 					sudo('unzip %s' % _PACKAGE_URL.split('/')[-1])
+					sudo('wget --no-clobber %s' % _MASTER_URL)
+					sudo('unzip %s' % _MASTER_FILE)
 
 	def config(self):
 		# Possible configuration options are custom repositories by setting the repositories setting to an array of repository objects
@@ -98,8 +105,8 @@ class Snowplow(Service):
 			repositories = self.settings.get('repositories')
 			if repositories:
 				resolver['data']['repositories'].extend(repositories)
-			put(StringIO.StringIO(json.dumps(resolver, indent=4)), _RESOLVER_PATH, use_sudo=True, mode=0644)
-			put(self.settings['config_file'], _CONFIG_PATH, use_sudo=True, mode=0644)
+			chown(put(StringIO.StringIO(json.dumps(resolver, indent=4)), _RESOLVER_PATH, use_sudo=True, mode=0644))
+			chown(put(self.settings['config_file'], _CONFIG_PATH, use_sudo=True, mode=0644))
 
 			# Read the config file for storage configuration
 			with open(self.settings['config_file']) as f:
@@ -110,5 +117,16 @@ class Snowplow(Service):
 							_config_postgres(target)
 
 			# Schedule cron jobs
-			# remove_crontab_section(_CRONTAB_USER, _CRONTAB_SECTION)
-			# add_crontab_section(_CRONTAB_USER, _CRONTAB_SECTION, )
+			loader_skip = ','.join(self.settings.get('loader_skip', _DEFAULT_SETTINGS['loader_skip']))
+			if loader_skip:
+				loader_skip = '--skip ' + loader_skip
+			loader_job = CronJob(_LOADER_COMMAND + loader_skip, log_name=self.name, chain=self.settings.get('chain', _DEFAULT_SETTINGS['chain']))
+			runner_skip = ','.join(self.settings.get('runner_skip', _DEFAULT_SETTINGS['runner_skip']))
+			if runner_skip:
+				runner_skip = '--skip ' + runner_skip
+			runner_enrichments = self.settings.get('enrichments', _DEFAULT_SETTINGS['enrichments'])
+			if runner_enrichments:
+				runner_enrichments = '--enrichments ' + runner_enrichments
+			runner_job = CronJob(_RUNNER_COMMAND + runner_enrichments + runner_skip, log_name=self.name, schedule=self.settings.get('schedule', _DEFAULT_SETTINGS['schedule']), chain=loader_job)
+			remove_crontab_section(_CRONTAB_USER, _CRONTAB_SECTION)
+			add_crontab_section(_CRONTAB_USER, _CRONTAB_SECTION, runner_job)
