64e8de28ccd0288b492f603eae17bd2d52b72783
marc <marc@symbioticsoftware.com>
2016-12-28 01:55:13 -0800

    Updated config process for postgres.

--- shuttle/services/postgres.py
+++ shuttle/services/postgres.py
@@ -1,97 +1,92 @@
 from importlib import import_module
-import tempfile
+import StringIO
 
-from fabric.api import run, sudo, cd, put, env, settings
+from fabric.api import run, sudo, cd, put, env, settings, hide
 from fabric.context_managers import shell_env
-from fabric.contrib.files import append
+from fabric.contrib.files import append, exists
 
+from .postgis import *
 from .service import Service
 from ..hooks import hook
 from ..shared import apt_get_install, pip_install, find_service, chown
 
+_CONFIG_FILE_PATH = '$(sudo -u postgres psql -t -P format=unaligned -c "SHOW config_file;")'
+_HBA_FILE_PATH = '$(sudo -u postgres psql -t -P format=unaligned -c "SHOW hba_file;")'
+_IDENT_FILE_PATH = '$(sudo -u postgres psql -t -P format=unaligned -c "SHOW ident_file;")'
+_MAIN_DIR = '$(dirname %s)' % _CONFIG_FILE_PATH
+_CONF_DIR = '%s/conf.d' % _MAIN_DIR
 _POSTGRES_USER = 'postgres'
+_POSTGRES_GROUP = 'postgres'
+_EXCLUDE_SETTINGS = ['postgis', 'hba', 'ident']
+
+def _pg_quote_config(key, value):
+	if (isinstance(value, (str, unicode)) and value not in ('on', 'off') and not value[0].isdigit()) or key == 'listen_addresses':
+		return "'%s'" % value
+	return value
 
 class Postgres(Service):
 	name = 'postgres'
 	script = 'postgresql'
 
-	def __init__(self, settings=None, postgis=False):
-		self.settings = settings
-		self.postgis = postgis
+	def run_sql(raw_sql, site):
+		sql = []
+		for line in raw_sql.split('\n'):
+			line = line.strip()
+			if not line or line.startswith('--'):
+				continue
+			sql.append(line.replace('\t', '').replace('\n', '').replace("'", "\\'"))
+		sql = ' '.join(sql)
+		sudo("psql -c $'%s'" % sql)
 
 	def install(self):
 		with hook('install %s' % self.name, self):
 			apt_get_install('postgresql')
-			if self.postgis:
-				self.install_postgis()
-
-	def install_postgis(self):
-		apt_get_install('postgresql-server-dev-all', 'libpq-dev', 'libxml2', 'libxml2-dev')
-
-		# Install geos
-		run('wget --no-clobber http://archive.ubuntu.com/ubuntu/pool/universe/g/geos/geos_3.3.3.orig.tar.gz')
-		run('tar -xzf geos_3.3.3.orig.tar.gz')
-		with cd('geos-3.3.3'):
-			run('./configure')
-			run('make')
-			sudo('make install')
-
-		# Install proj.4
-		# https://docs.djangoproject.com/en/dev/ref/contrib/gis/install/
-		run('wget --no-clobber http://download.osgeo.org/proj/proj-4.8.0.tar.gz')
-		run('wget --no-clobber http://download.osgeo.org/proj/proj-datumgrid-1.5.tar.gz')
-		run('tar -xzf proj-4.8.0.tar.gz')
-		with cd('proj-4.8.0/nad'):
-			run('tar -xzf ../../proj-datumgrid-1.5.tar.gz')
-			with cd('..'):
-				run('./configure')
-				run('make')
-				sudo('make install')
-
-		# Install gdal
-		run('wget --no-clobber http://download.osgeo.org/gdal/gdal-1.9.2.tar.gz')
-		run('tar -xzf gdal-1.9.2.tar.gz')
-		with cd('gdal-1.9.2'):
-			run('./configure')
-			run('make')
-			sudo('make install')
-			sudo('ldconfig')
-
-		run('wget --no-clobber http://download.osgeo.org/postgis/source/postgis-2.0.3.tar.gz')
-		run('tar -xzf postgis-2.0.3.tar.gz')
-		with cd('postgis-2.0.3'):
-			run('./configure')
-			run('make')
-			sudo('make install')
+			if self.settings.get('postgis'):
+				install_postgis()
 
 	def config(self):
 		with hook('config %s' % self.name, self):
+			if not exists(_CONF_DIR):
+				sudo('mkdir %s' % _CONF_DIR)
+				chown(_CONF_DIR, _POSTGRES_USER, _POSTGRES_GROUP)
+				append(_CONFIG_FILE_PATH, "include_dir 'conf.d'", use_sudo=True)
 			if self.settings:
-				with tempfile.NamedTemporaryFile('w') as f:
-					for setting in self.settings:
-						f.write('%s = %s\n' % (setting, self.settings[setting]))
-					f.flush()
-					chown(put(f.name, '/etc/postgresql/9.1/main/fabric.conf', use_sudo=True, mode=0644), _POSTGRES_USER, _POSTGRES_USER)
-					append('/etc/postgresql/9.1/main/postgresql.conf', "\n\ninclude 'fabric.conf'\n", use_sudo=True)
+				# Apply any given settings and place into a new conf.d directory
+				config = ''
+				for setting in self.settings:
+					if setting not in _EXCLUDE_SETTINGS:
+						config += '%s = %s\n' % (setting, _pg_quote_config(setting, self.settings[setting]))
+				if config:
+					chown(put(StringIO.StringIO(config), _CONF_DIR + '/fabric.conf', use_sudo=True, mode=0644), _POSTGRES_USER, _POSTGRES_GROUP)
+				# Apply any given Client Authentications given under hba
+				hba = list(self.settings.get('hba', []))
+				if env.get('vagrant'):
+					with hide('everything'):
+						host_ip = run('echo $SSH_CLIENT').split(' ')[0]
+					hba.append(('host', 'all', 'all', host_ip + '/32', 'md5'))
+				if hba:
+					append(_HBA_FILE_PATH, '# Fabric client connections:', use_sudo=True)
+				for client in hba:
+					client = '%s%s%s%s%s' % (client[0].ljust(8), client[1].ljust(16), client[2].ljust(16), client[3].ljust(24), client[4])
+					append(_HBA_FILE_PATH, client, use_sudo=True)
+				# Apply any given identity mappings
+				ident = self.settings.get('ident', [])
+				for mapping in ident:
+					mapping = '%s%s%s' % (mapping[0].ljust(16), mapping[1].ljust(24), mapping[2])
+					append(_IDENT_FILE_PATH, mapping, use_sudo=True)
 		self.restart()
 
 	def site_install(self, site):
 		with hook('site install %s' % self.name, self, site):
-			if self.postgis:
+			if self.settings.get('postgis'):
 				# Install PostGIS also on the site if separate from the server
 				if find_service(self.name) is None:
-					self.install_postgis()
+					install_postgis()
 			# Install python postgresql support
 			apt_get_install('python-dev', 'postgresql-server-dev-all', 'postgresql-client')
 			pip_install(site, 'psycopg2')
 
 	def site_config(self, site):
-		answer = raw_input('Running this postgres site configuration will drop any existing database. Are you sure? (Y/N): ')
-		if answer.lower() != 'y':
-			return
-		answer = raw_input('Are you absolutely sure? (Y/N): ')
-		if answer.lower() != 'y':
-			return
 		with hook('site config %s' % self.name, self, site):
 			# Create the user for django to access the database with
 			module = import_module(site['settings_module'])
@@ -112,13 +107,6 @@ class Postgres(Service):
 					sudo("psql -c \"ALTER USER %s WITH PASSWORD '%s';\"" % (DATABASES['default']['USER'], DATABASES['default']['PASSWORD']), user=user)
 					sudo('createuser --createdb --no-superuser --no-createrole %s' % env.user, user=user)
 			with shell_env(PGPASSWORD=DATABASES['default']['PASSWORD']):
-				with settings(warn_only=True):
-					sudo('dropdb %s %s' % (connect_args, DATABASES['default']['NAME']), user=user)
 				sudo('createdb %s %s' % (connect_args, DATABASES['default']['NAME']), user=user)
-				if self.postgis:
-					sudo("psql %s %s -c 'CREATE EXTENSION postgis;'" % (connect_args, DATABASES['default']['NAME']), user=user)
-					sudo("psql %s %s -c 'CREATE EXTENSION postgis_topology;'" % (connect_args, DATABASES['default']['NAME']), user=user)
-					# http://tmbu.blogspot.com/2012/11/postgres-postgis-django-os-x-and.html
-					# http://stackoverflow.com/questions/4737982/django-avoids-creating-pointfield-in-the-database-when-i-run-python-manage-py-sy
-					sudo("psql %s %s -c 'GRANT ALL PRIVILEGES ON geometry_columns TO %s;'" % (connect_args, DATABASES['default']['NAME'], DATABASES['default']['USER']), user=user)
-					sudo("psql %s %s -c 'GRANT ALL PRIVILEGES ON spatial_ref_sys TO %s;'" % (connect_args, DATABASES['default']['NAME'], DATABASES['default']['USER']), user=user)
+				if self.settings.get('postgis'):
+					site_config_postgis(connect_args)
