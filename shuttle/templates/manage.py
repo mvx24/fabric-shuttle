af9cb9441a558c8f06816614cd38c1b49809e288
marc <marc@symbioticsoftware.com>
2017-01-18 22:25:42 -0800

    Updated manage.py templates to cd into the project directory for correct path resolution in settings.

--- shuttle/templates/manage.py
+++ shuttle/templates/manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == '__main__':
-	# NOTE: os.chdir() to the project does not work so instead add to sys.path
+	# NOTE: os.chdir() to the project does not work for loading up the settings module so instead add to sys.path
+	# os.chdir is included however so that usage of os.path.abspath('.') will resolve correct
+	os.chdir('%(project_dir)s')
 	sys.path.append('%(project_dir)s')
 	os.environ.setdefault('DJANGO_SETTINGS_MODULE', '%(settings_module)s')
 	from django.core.management import execute_from_command_line
