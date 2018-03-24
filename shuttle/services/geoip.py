8309eae1c4ee7dd7a8c17ee04744ea0d43dab0e9
marc <marc@symbioticsoftware.com>
2018-03-24 00:12:18 -0700

    Changed indentation.

--- shuttle/services/geoip.py
+++ shuttle/services/geoip.py
@@ -11,23 +11,23 @@ _COUNTRY_PACKAGE_URL = 'http://geolite.maxmind.com/download/geoip/database/GeoLi
 _INSTALL_DIR = '/opt/geoip'
 
 class GeoIP(Service):
-	"""Database to convert ip addresses into locations."""
-	name = 'geoip'
-	script = None
+    """Database to convert ip addresses into locations."""
+    name = 'geoip'
+    script = None
 
-	def install(self):
-		with hook('install %s' % self.name, self):
-			if not exists(_INSTALL_DIR):
-				apt_get_install('libgeoip1')
-				sudo('mkdir %s' % _INSTALL_DIR)
-				with cd(_INSTALL_DIR):
-					sudo('wget --no-clobber %s' % _CITY_PACKAGE_URL)
-					sudo('wget --no-clobber %s' % _COUNTRY_PACKAGE_URL)
-					sudo('gunzip %s' % _CITY_PACKAGE_URL.split('/')[-1])
-					sudo('gunzip %s' % _COUNTRY_PACKAGE_URL.split('/')[-1])
+    def install(self):
+        with hook('install %s' % self.name, self):
+            if not exists(_INSTALL_DIR):
+                apt_get_install('libgeoip1')
+                sudo('mkdir %s' % _INSTALL_DIR)
+                with cd(_INSTALL_DIR):
+                    sudo('wget --no-clobber %s' % _CITY_PACKAGE_URL)
+                    sudo('wget --no-clobber %s' % _COUNTRY_PACKAGE_URL)
+                    sudo('gunzip %s' % _CITY_PACKAGE_URL.split('/')[-1])
+                    sudo('gunzip %s' % _COUNTRY_PACKAGE_URL.split('/')[-1])
 
-	def get_site_settings(self, site):
-		if site['type'] == SiteType.DJANGO:
-			return { 'GEOIP_PATH': _INSTALL_DIR }
-		else:
-			return super(GeoIP, self).get_site_settings(site)
+    def get_site_settings(self, site):
+        if site['type'] == SiteType.DJANGO:
+            return { 'GEOIP_PATH': _INSTALL_DIR }
+        else:
+            return super(GeoIP, self).get_site_settings(site)
--- shuttle/services/postgis.py
+++ shuttle/services/postgis.py
@@ -13,49 +13,49 @@ _POSTGIS_URL = 'http://download.osgeo.org/postgis/source/postgis-2.0.3.tar.gz'
 _POSTGIS_DIR = 'postgis-2.0.3'
 
 def install_postgis():
-	apt_get_install('postgresql-server-dev-all', 'libpq-dev', 'libxml2', 'libxml2-dev')
+    apt_get_install('postgresql-server-dev-all', 'libpq-dev', 'libxml2', 'libxml2-dev')
 
-	# Install geos
-	run('wget --no-clobber %s' % _GEOS_URL)
-	run('tar -xzf %s' % _GEOS_URL.split('/')[-1])
-	with cd(_GEOS_DIR):
-		run('./configure')
-		run('make')
-		sudo('make install')
+    # Install geos
+    run('wget --no-clobber %s' % _GEOS_URL)
+    run('tar -xzf %s' % _GEOS_URL.split('/')[-1])
+    with cd(_GEOS_DIR):
+        run('./configure')
+        run('make')
+        sudo('make install')
 
-	# Install proj.4
-	# https://docs.djangoproject.com/en/dev/ref/contrib/gis/install/
-	run('wget --no-clobber %s' % _PROJ4_URL)
-	run('wget --no-clobber %s' % _PROJ4_DATUM_URL)
-	run('tar -xzf %s' % _PROJ4_URL.split('/')[-1])
-	with cd(_PROJ4_DIR):
-		run('tar -xzf ../../%s' % _PROJ4_DATUM_URL.split('/')[-1])
-		with cd('..'):
-			run('./configure')
-			run('make')
-			sudo('make install')
+    # Install proj.4
+    # https://docs.djangoproject.com/en/dev/ref/contrib/gis/install/
+    run('wget --no-clobber %s' % _PROJ4_URL)
+    run('wget --no-clobber %s' % _PROJ4_DATUM_URL)
+    run('tar -xzf %s' % _PROJ4_URL.split('/')[-1])
+    with cd(_PROJ4_DIR):
+        run('tar -xzf ../../%s' % _PROJ4_DATUM_URL.split('/')[-1])
+        with cd('..'):
+            run('./configure')
+            run('make')
+            sudo('make install')
 
-	# Install gdal
-	run('wget --no-clobber %s' % _GDAL_URL)
-	run('tar -xzf %s' % _GDAL_URL.split('/')[-1])
-	with cd(_GDAL_DIR):
-		run('./configure')
-		run('make')
-		sudo('make install')
-		sudo('ldconfig')
+    # Install gdal
+    run('wget --no-clobber %s' % _GDAL_URL)
+    run('tar -xzf %s' % _GDAL_URL.split('/')[-1])
+    with cd(_GDAL_DIR):
+        run('./configure')
+        run('make')
+        sudo('make install')
+        sudo('ldconfig')
 
-	run('wget --no-clobber %s' % _POSTGIS_URL)
-	run('tar -xzf %s' % _POSTGIS_URL.split('/')[-1])
-	with cd(_POSTGIS_DIR):
-		run('./configure')
-		run('make')
-		sudo('make install')
+    run('wget --no-clobber %s' % _POSTGIS_URL)
+    run('tar -xzf %s' % _POSTGIS_URL.split('/')[-1])
+    with cd(_POSTGIS_DIR):
+        run('./configure')
+        run('make')
+        sudo('make install')
 
 def site_config_postgis(postgres, site):
-	database = get_django_setting(site, 'DATABASES')['default']
-	postgres.execute_sql('CREATE EXTENSION postgis;', site)
-	postgres.execute_sql('CREATE EXTENSION postgis_topology;', site)
-	# http://tmbu.blogspot.com/2012/11/postgres-postgis-django-os-x-and.html
-	# http://stackoverflow.com/questions/4737982/django-avoids-creating-pointfield-in-the-database-when-i-run-python-manage-py-sy
-	postgres.execute_sql('GRANT ALL PRIVILEGES ON geometry_columns TO %s;' % database['USER'], site)
-	postgres.execute_sql('GRANT ALL PRIVILEGES ON spatial_ref_sys TO %s;' % database['USER'], site)
+    database = get_django_setting(site, 'DATABASES')['default']
+    postgres.execute_sql('CREATE EXTENSION postgis;', site)
+    postgres.execute_sql('CREATE EXTENSION postgis_topology;', site)
+    # http://tmbu.blogspot.com/2012/11/postgres-postgis-django-os-x-and.html
+    # http://stackoverflow.com/questions/4737982/django-avoids-creating-pointfield-in-the-database-when-i-run-python-manage-py-sy
+    postgres.execute_sql('GRANT ALL PRIVILEGES ON geometry_columns TO %s;' % database['USER'], site)
+    postgres.execute_sql('GRANT ALL PRIVILEGES ON spatial_ref_sys TO %s;' % database['USER'], site)
